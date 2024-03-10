a66ab166e0bb4b9b29a9a49c1c9f1d11cb9cf310
Ajay Jain <njain9104@gmail.com>
2024-03-10 08:57:46 +0530

    feat: modified logic to support toc on all sites

--- content_script/main.ts
+++ content_script/main.ts
@@ -1,50 +1,26 @@
 import { Headers, Message, MessageTypes } from "../shared/types";
-
-// Ignore h1 as it is title
-const headerTags = ["h2","h3","h4","h5","h6"].map(h => `${h}[data-selectable-paragraph]`).join(',')
+import { onGetClipboardHTML, onNavigateToHeader, onOpenPopup } from "./onMessageHandlers";
 
 const registerEventListeners = () => {
     chrome.runtime.onMessage.addListener((message:Message,_sender,sendResponse) => {
         switch(message.type){
             case MessageTypes.OPEN_POPUP:{
-                const headers = document.querySelectorAll(headerTags);
-                if(headers && headers.length > 0){
-                    const headerDetails:Headers = [];
-                    headers.forEach(({id,textContent,tagName,classList}) => {
-                        // Filter out subtitle
-                        if(!classList.contains("pw-subtitle-paragraph")){
-                            headerDetails.push({id,textContent,tagName})
-                        }
-                    })
-                    sendResponse({headers:headerDetails})
-                }    
+                const headers = onOpenPopup();
+                sendResponse({headers})
+                break;
             }
-            break;
-            case MessageTypes.NAVIGATE_TO_HEADER:{
-                location.hash = message.body?.id as string
-            }
-            break;
+            case MessageTypes.NAVIGATE_TO_HEADER:
+                onNavigateToHeader(message.body?.id as string);
+                break;
             case MessageTypes.GET_CLIPBOARD_HTML:{
-                if(message.body?.headers){
-                    const headers = message.body.headers as Headers
-                    const root = document.createElement("div");
-                    headers.forEach(header => {
-                      const anchor = document.createElement("a");
-                      anchor.setAttribute("href",location.origin+location.pathname+`#${header.id}`);
-                      anchor.innerText = header.textContent as string;
-                      root.appendChild(anchor);
-                      root.appendChild(document.createElement("br"));
-                    })
-                    sendResponse({html:root.innerHTML})
-                } 
+                const html = onGetClipboardHTML(message.body?.headers as Headers);
+                sendResponse({html})
                 break;         
             }
         }
     })
 }
 
-// Called from dynamic import
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 (() => {
     registerEventListeners();
 })();
---
+++ content_script/onMessageHandlers.ts
@@ -0,0 +1,47 @@
+import { Headers } from "../shared/types";
+
+// Ignore h1 as it is title
+const headerTags = ["h2","h3","h4","h5","h6","footer"].join(',');
+
+const RESTRICTED_CLASSLISTS = ['pw-subtitle-paragraph'];
+
+
+export const onOpenPopup = () => {
+    const headers = document.querySelectorAll(headerTags);
+    const headerDetails:Headers = [];
+
+    if(headers && headers.length > 0){
+        for(let header of headers){
+            const {id,textContent,tagName,classList} = header;
+            if(tagName === 'footer') break;
+            if(id){
+                const classListSet = new Set(classList);
+                RESTRICTED_CLASSLISTS.forEach(cl => {
+                    if(!classListSet.has(cl)){
+                        headerDetails.push({id,textContent: (header as HTMLElement).innerText || textContent,tagName});
+                    }
+                })
+            }
+        }
+    }
+    
+    return headerDetails;
+}
+
+export const onNavigateToHeader = (id:string) => {
+    location.hash = id;
+}
+
+export const onGetClipboardHTML = (headers=[] as Headers) => {
+    const root = document.createElement("div");
+    
+    headers.forEach(header => {
+      const anchor = document.createElement("a");
+      anchor.setAttribute("href",location.origin+location.pathname+`#${header.id}`);
+      anchor.innerText = header.textContent as string;
+      root.appendChild(anchor);
+      root.appendChild(document.createElement("br"));
+    })
+
+    return root.innerHTML;
+}
